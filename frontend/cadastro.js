e2241e70f1ad5e739d5ec707e260e224594adcc7
gabrieldsa <gabrieldsa@gmail.com>
2025-10-19 21:52:37 -0300

    feat: Adiciona funcionalidade de excluir e  editar produto

--- frontend/cadastro.js
+++ frontend/cadastro.js
@@ -50,10 +50,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 mensagemElement.innerHTML = `<p style="color: green;">Produto <strong>${data.nome}</strong> cadastrado com sucesso!</p>`;
                 formCadastro.reset();
                 
-                // AJUSTE: Limpa a mensagem após 3 segundos
+                // AJUSTE: Limpa a mensagem após 10 segundos
                 setTimeout(() => {
                     mensagemElement.innerHTML = '';
-                }, 18000); // 18000 milissegundos = 18 segundos
+                }, 10000); // 10000 milissegundos = 10 segundos
                 
             })
             .catch(error => {
